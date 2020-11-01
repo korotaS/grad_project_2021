217ff70a70e7e74744c7876ba6900209d2985c28
korotaS <askorotaevskiy@sberbank.ru>
2020-11-01 16:16:00 +0300

    custom datasets added; some work on train.py workflow

--- src/python/train.py
+++ src/python/train.py
@@ -2,12 +2,18 @@ import json
 from threading import Thread
 from time import sleep
 import os
+import shutil
 
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
 from torch import optim
 from torchvision import datasets, models, transforms
+
+try:
+    from src.python.utils.datasets import ImageClassificationDataset
+except ImportError:
+    from utils.datasets import ImageClassificationDataset
 
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -17,20 +23,51 @@ class TrainThread(Thread):
     def __init__(self, data):
         super().__init__()
         self.status = 'NOT INITIALIZED'
-        self.dataset_folder = data['datasetFolder']
+        self.raw_dataset_folder = data['datasetFolder']
+
         self.project_name = data['projectName']
         if not os.path.exists('./projects/'):
             os.mkdir('./projects/')
-        self.project_folder = os.path.join('./projects/', project_name)
-        self.data_folder = os.path.join(self.project_folder, 'dataset/')
+
+        self.project_folder = os.path.join('./projects/', self.project_name)
         if not os.path.exists(self.project_folder):
             os.mkdir(self.project_folder)
-        if not os.path.exists(self.data_folder):
-            os.mkdir(self.data_folder)
+
+        data_folder = os.path.join(self.project_folder, 'dataset/')
+        if not os.path.exists(data_folder):
+            os.mkdir(data_folder)
+        self.train_folder = os.path.join(data_folder, 'train/')
+        self.test_folder = os.path.join(data_folder, 'test/')
+
+        self.copy_data()
+
+        self.train_dataset, self.test_dataset, self.train_loader, self.test_loader = self.init_datasets()
         self.status = 'INITIALIZED'
 
     def run(self):
         self.train()
+
+    def copy_data(self):
+        if os.path.exists(self.train_folder):
+            shutil.rmtree(self.train_folder)
+        shutil.copytree(os.path.join(self.raw_dataset_folder, 'train/'), self.train_folder)
+
+        if os.path.exists(self.test_folder):
+            shutil.rmtree(self.test_folder)
+        shutil.copytree(os.path.join(self.raw_dataset_folder, 'test/'), self.test_folder)
+
+    def init_datasets(self):
+        tr_dataset = ImageClassificationDataset(self.train_folder)
+        te_dataset = ImageClassificationDataset(self.test_folder)
+        tr_loader = torch.utils.data.DataLoader(
+            tr_dataset,
+            batch_size=32,
+            shuffle=True)
+        te_loader = torch.utils.data.DataLoader(
+            te_dataset,
+            batch_size=32,
+            shuffle=True)
+        return tr_dataset, te_dataset, tr_loader, te_loader
 
     def write_log(self, data):
         with open(f'{self.project_folder}/log.json', 'w+') as w:
@@ -41,41 +78,38 @@ class TrainThread(Thread):
             return json.load(r)
 
     def train(self):
-        try:
-            dataset_class = getattr(datasets, self.dataset_folder)
-        except AttributeError:
-            dataset_class = datasets.MNIST
-
-        train_loader = torch.utils.data.DataLoader(
-            dataset_class(self.data_folder, train=True, download=True, transform=transforms.Compose([
-                transforms.ToTensor(),
-            ])),
-            batch_size=32,
-            shuffle=True)
-
         model = models.mobilenet_v2(pretrained=True)
         model.classifier[1] = nn.Linear(model.classifier[1].in_features, 10)
 
         self.write_log({'project': self.project_folder, 'epochs': [], 'status': 'training'})
 
         model.train()
-        optimizer = optim.Adam(model.parameters(), lr=0.001)
-        NUM_BATCHES = 10
+        optimizer = optim.Adam(model.parameters(), lr=0.0001)
+        NUM_TR_BATCHES = 10
+        NUM_TE_BATCHES = 5
 
         for epoch in range(10):
             epoch_loss = 0
-            for batch, (data, target) in enumerate(train_loader):
-                if batch >= NUM_BATCHES:
+            for batch, (data, target) in enumerate(self.train_loader):
+                if batch >= NUM_TR_BATCHES:
                     break
                 optimizer.zero_grad()
                 output = model(data)
                 loss = F.cross_entropy(output, target)
-                epoch_loss += loss.item()
                 loss.backward()
                 optimizer.step()
-                print(epoch, batch, epoch_loss)
-
-            epoch_loss /= NUM_BATCHES
+                print(f'train: epoch {epoch}, batch {batch}, loss {loss.item()}')
+            print()
+            with torch.no_grad():
+                for batch, (data, target) in enumerate(self.test_loader):
+                    if batch >= NUM_TE_BATCHES:
+                        break
+                    output = model(data)
+                    loss = F.cross_entropy(output, target)
+                    epoch_loss += loss.item()
+                    print(f'test: epoch {epoch}, batch {batch}, loss {loss.item()}')
+            print()
+            epoch_loss /= NUM_TE_BATCHES
 
             epochs = self.read_log()
             epochs['epochs'].append({'loss': epoch_loss,
@@ -86,3 +120,7 @@ class TrainThread(Thread):
         epochs = self.read_log()
         epochs['status'] = 'done'
         self.write_log(epochs)
+
+
+thread = TrainThread({'projectName': 'project_1', 'datasetFolder': './projects/datasets/dataset_cifar/'})
+thread.start()
---
+++ src/python/utils/datasets.py
@@ -0,0 +1,56 @@
+import os
+
+import torch
+from torch.utils.data import Dataset
+
+
+class BaseDataset(Dataset):
+    def __len__(self):
+        super().__init__()
+        pass
+
+    def __getitem__(self, item):
+        pass
+
+    def check_structure(self):
+        pass
+
+    def check_content(self):
+        pass
+
+
+class ImageClassificationDataset(BaseDataset):
+    def __init__(self, path, device='cpu'):
+        super().__init__()
+        self.path = path
+        self.device = device
+        self.check_structure()
+        self.data = torch.load(os.path.join(self.path, 'data.pt')).to(self.device)
+        self.labels = torch.load(os.path.join(self.path, 'labels.pt')).to(self.device)
+
+    def check_structure(self):
+        if not os.path.exists(os.path.join(self.path, 'data.pt')) or \
+           not os.path.exists(os.path.join(self.path, 'labels.pt')):
+            raise DatasetStructureError(f"folder {self.path} doesn\'t have data.pt or labels.pt")
+
+    def check_content(self):
+        if len(self.data) != len(self.labels):
+            raise DatasetContentError('Data and labels have different lengths')
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        return self.data[idx], self.labels[idx]
+
+
+class DatasetStructureError(ValueError):
+    """Raised when structure of dataset folder is incorrect"""
+    pass
+
+
+class DatasetContentError(ValueError):
+    """Raised when structure of dataset folder is incorrect"""
+    pass
