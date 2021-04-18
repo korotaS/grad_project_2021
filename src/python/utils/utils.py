b569bbb5007e3b14491939ecd5c9f91d76811e76
korotaS <korota-sasha@bk.ru>
2021-04-18 20:53:42 +0300

    GP-29 - multiclass segmentation is working, also visualization; rle updated

--- src/python/utils/utils.py
+++ src/python/utils/utils.py
@@ -28,9 +28,13 @@ def rle_encode(seq):
     return values, counts
 
 
-def rle_encode_mask(mask):
+def rle_encode_mask(mask, return_dict=False):
     flatten_mask = np.array(mask).flatten()
     values, counts = rle_encode(flatten_mask)
+    if return_dict:
+        return {'values': list([int(v) for v in values]),
+                'counts': list([int(c) for c in counts]),
+                'shape': mask.shape}
     return values, counts, mask.shape
 
 
@@ -41,10 +45,22 @@ def rle_decode(values, counts):
     return np.array(seq)
 
 
-def rle_decode_mask(values, counts, image_shape):
-    h, w = image_shape[:2]
+def rle_decode_mask_raw(rle_dict):
+    values = np.array(rle_dict['values'])
+    counts = np.array(rle_dict['counts'])
+    h, w = rle_dict['shape'][:2]
     seq = rle_decode(values, counts)
     mask = np.reshape(seq, (w, h))
+    return mask
+
+
+def rle_decode_mask(full_rle_dict):
+    shapes = [tuple(value['shape']) for value in full_rle_dict.values()]
+    assert len(set(shapes)) == 1, 'Shape of all channels in the mask must be equal.'
+    shape = shapes[0]
+    mask = np.zeros(shape+(len(full_rle_dict),))
+    for i, value in enumerate(full_rle_dict.values()):
+        mask[:, :, i] = rle_decode_mask_raw(value)
     return mask
 
 
